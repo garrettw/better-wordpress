a25cd83b1aec03c0ba0be7f056d179463e439fc2
Garrett W. <garrettw87@gmail.com>
2023-10-04 05:01:30 +0000

    +wonolog, stage switcher; tweak .gitignore

---
+++ web/app/mu-plugins/wonolog.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:  Wonolog
+Plugin URI:   https://inpsyde.github.io/Wonolog/
+Description:  Monolog-based logging package for WordPress
+Version:      2.0.0-beta.2
+Author:       inpsyde
+Author URI:   https://inpsyde.com/
+License:      MIT License
+*/
+Inpsyde\Wonolog\bootstrap();
